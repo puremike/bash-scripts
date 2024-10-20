8d2d6782d5c75649c303f99c16d2d27496b97342
puremike <michaelmarketing25@gmail.com>
2024-10-20 11:29:49 +0100

    Made update on the 05exercise.sh script

--- section-1/05exercise.sh
+++ section-1/05exercise.sh
@@ -7,41 +7,39 @@ if [[ $UID -ne 0 ]]; then
 fi
 
 # Get the username (login).
-read -p "Enter username to create user account: " user_name
+read -p "Enter username to create user account: " USER_NAME
 
 # Get the real name (contents for the description field).
-read -p "Enter real name for comment: " comment
+read -p "Enter real name for comment: " COMMENT
 
 # Get the password.
-read -p "Enter password for user account: " password
+read -p "Enter password for user account: " PASSWORD
 
 # Create the user with the password.
-useradd -c "$comment" -m $user_name
+useradd -c "$COMMENT" -m $USER_NAME
 
 # Check to see if the useradd command succeeded.
-if [[ $? -eq 0 ]]; then
-    echo "<useradd> command was successful"
-else
-    echo "<useradd> command failed!"
+if [[ $? -ne 0 ]]; then
+    echo "user creation failed"
     exit 1
 fi
 
 # Set the password.
-echo "$user_name:$password" | chpasswd
+echo "$USER_NAME:$PASSWORD" | chpasswd
 
 # Check to see if the passwd command succeeded.
-if [[ $? -eq 0 ]]; then
-    echo "<passwd> command was successful"
-else
-    echo "<passwd> command failed!"
+if [[ $? -ne 0 ]]; then
+    echo "Password can't be set!"
     exit 1
 fi
 
 # Force password change on first login.
-passwd -e $user_name
+passwd -e $USER_NAME
 
 # Display the username, password, and the host where the user was created.
-echo $'\n'
-echo "Username: $user_name"
-echo "Password: $password"
-echo "Host: $(hostname)"
+echo
+echo "Username: $USER_NAME"
+echo "Password: $PASSWORD"
+echo "Host: $HOSTNAME"
+
+exit 0
